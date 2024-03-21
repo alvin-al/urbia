62bdca011b2d7327910af5c0dba2bbc3834bede0
Alvin Al <alvvxal@gmail.com>
2024-03-21 07:36:37 +0700

    Modify desktop view

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,4 +1,5 @@
 'use client'
+
 import Image from "next/image"
 import {
   Carousel,
@@ -12,7 +13,7 @@ import pic1 from '@/../public/images/AJ1.webp'
 import pic2 from '@/../public/images/Giorno.png'
 import pic3 from '@/../public/images/living room lt.1.webp'
 import pic4 from '@/../public/images/XT NOON 3.webp'
-import Autoplay from 'embla-carousel-autoplay'
+import Autoplay from 'embla-carousel-autoplay'  
 
 const DemoCarousel = () => {
   const listOfPic = [
@@ -36,20 +37,20 @@ const DemoCarousel = () => {
 
 
   return (
-    <div className="rounded-lg h-[60vh] border">
+    <div className="rounded-lg h-[70vh] lg:h-[75vh] my-2">
       <Carousel 
         opts={{
           loop: true,
           }} 
         plugins={[
             Autoplay({
-              delay: 2000,
+              delay: 1000,
             }),
           ]}>
         <CarouselContent>
-          <CarouselItem><Image src={pic1} alt="" className="rounded-lg object-cover h-[60vh]"/></CarouselItem>
-          <CarouselItem><Image src={pic2} alt="" className="rounded-lg object-cover h-[60vh]"/></CarouselItem>
-          <CarouselItem><Image src={pic3} alt="" className="rounded-lg object-cover h-[60vh]"/></CarouselItem>
+          <CarouselItem><Image src={pic1} alt="" className="rounded-lg object-cover h-[70vh] lg:h-[75vh] w-full"/></CarouselItem>
+          <CarouselItem><Image src={pic2} alt="" className="rounded-lg object-cover h-[70vh] lg:h-[75vh] w-full"/></CarouselItem>
+          <CarouselItem><Image src={pic3} alt="" className="rounded-lg object-cover h-[70vh] lg:h-[75vh] w-full object-bottom"/></CarouselItem>
         </CarouselContent>
       </Carousel>
       
