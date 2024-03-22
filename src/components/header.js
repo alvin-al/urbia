bc61a8799834d61aa8185b1e1bb163b3f4c1954b
Alvin Al <alvvxal@gmail.com>
2024-03-22 07:42:46 +0700

    Add overlay menu

--- src/components/header.js
+++ src/components/header.js
@@ -2,29 +2,32 @@
 import React, { useState } from 'react';
 import Image from "next/image";
 import logo from '@/../public/images/logo.svg'
-import HoverMenu from './hoverMenu';
+import OverlayMenu from './overlayMenu';
 
 
 const Header = () => {
   // const [isActive, setIsActive] = useState(false); 
   // const whenClicked = 'rotate-fade-off';
   const [isOpen, setIsOpen] = useState(false);
+
   const genericHamburgerLine = `h-[4px] w-6 my-[2px] rounded-full bg-black transition ease transform duration-300`;
 
   return (
-    <div className="flex h-[60px] items-center gap-2 ">
+    <div className="flex h-[60px] items-center gap-2 z-0">
       <button 
-        className="flex flex-col h-10 w-10 rounded justify-center items-center group hover:bg-slate-200 hover:fade-in-100 z-20" 
+        className={`${"flex flex-col h-10 w-10 rounded justify-center items-center group hover:fade-in-100 z-30"} ${isOpen ? "border-2 lg:hover:bg-gray-600" : "hover:bg-gray-200 "}`} 
         onClick={() => {
           setIsOpen(!isOpen);
         }}>
-        <div className={`${genericHamburgerLine} ${isOpen ? "rotate-45 translate-y-2 opacity-100" : "opacity-50 group-hover:opacity-100"}`}/>
+        
+        <div className={`${genericHamburgerLine} ${isOpen ? "rotate-45 translate-y-2 opacity-100 bg-white" : "opacity-50 group-hover:opacity-100 "}`}/>
         <div className={`${genericHamburgerLine} ${isOpen ? "opacity-0" : "opacity-50 group-hover:opacity-100"}`}/>
-        <div className={`${genericHamburgerLine} ${isOpen ? "-rotate-45 -translate-y-2 opacity-100" : "opacity-50 group-hover:opacity-100"}`}/>
+        <div className={`${genericHamburgerLine} ${isOpen ? "-rotate-45 -translate-y-2 opacity-100 bg-white" : "opacity-50 group-hover:opacity-100"}`}/>
       </button>
         <div>
             <a href=""><Image src={logo} alt='logo' sizes="(max-width : 375px) 80vw"/></a>
         </div>
+        {isOpen && <OverlayMenu open={isOpen} />}
     </div>
   )
 }
--- src/components/overlayMenu.jsx
+++ src/components/overlayMenu.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
-const OverlayMenu = (isOpen) => {
+const OverlayMenu = () => {
+  // Define the base styles for the overlay menu
+
+
+
   return (
-    <div>
-        
+    <div className="h-full w-full fixed z-10 left-0 top-0 bg-black overflow-x-hidden flex items-center justify-center pt-[15%]">
+      <ul className='flex flex-col text-white h-full w-full border-white'>
+        <li className='w-full h-1/4 flex justify-center items-center text-center text-4xl border-b'><a href="">Home</a></li>
+        <li className='w-full h-1/4 flex justify-center items-center text-center text-4xl border-b'><a href="">Projects</a></li>
+        <li className='w-full h-1/4 flex justify-center items-center text-center text-4xl border-b'><a href="">Services</a></li>
+        <li className='w-full h-1/4 flex justify-center items-center text-center text-4xl'><a href="">About Us</a></li>
+      </ul>
     </div>
   )
-}
+} 
 
 export default OverlayMenu
