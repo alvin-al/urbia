587c7edcc7ee55c4bdf14f544a449dc182491f18
Alvin Al <alvvxal@gmail.com>
2024-03-25 18:00:30 +0700

    Add typography styles

---
+++ src/components/styles.jsx
@@ -0,0 +1,12 @@
+// styles.js
+
+export const head = "font-extrabold text-4xl";
+export const headWeb = "font-semibold text-2xl";
+export const headTab = "font-semibold text-xl";
+export const headPhone = "font-semibold text-lg";
+export const servicesPoints = "font-semibold text-base";
+export const subheader = "font-medium text-sm";
+export const footer = "font-semibold text-sm";
+export const paragraph = "font-regular text-base";
+export const display = "font-semibold text-lg";
+export const displayPhone = "font-semibold text-base";
