d9a687ca83afbde9a0b08198fea4aab0df4be608
Alvin Al <alvvxal@gmail.com>
2024-03-25 18:00:00 +0700

    Add the title of page

--- src/homepage/page.js
+++ src/homepage/page.js
@@ -3,6 +3,7 @@ import Header from '@/components/header'
 import DemoCarousel from '@/components/carousel'
 import Social from '@/components/social'
 import Footer from '@/components/footer'
+import * as styles from '@/components/styles'
 
 
 const Homepage = () => {
@@ -10,18 +11,23 @@ const Homepage = () => {
     <div className="flex flex-col w-auto h-[95vh] px-6">
       <div>
         <div>
-        <Header />
+          <Header />
+        </div>
+        <div>
+          <p className={`${styles.headPhone} text-right md:${styles.headTab} lg:${styles.headWeb}`}>  
+            URBIA SPATIAL MANIFESTO
+          </p>
+        </div>
+        <div>
+          <DemoCarousel/>
+        </div>
       </div>
-      <div>
-        <DemoCarousel/>
-      </div>
-      </div>
-      <div>
-        <Social />
-      </div>
-      <div  className="absolute bottom-0">
-        <Footer/>
-      </div>
+        <div>
+          <Social />
+        </div>
+        <div  className="absolute bottom-0">
+          <Footer/>
+        </div>
     </div>
 
   )
