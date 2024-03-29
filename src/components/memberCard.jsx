d555bc113a34e644a5de00b3c99aff2c6bde8fb0
Alvin Al <alvvxal@gmail.com>
2024-03-29 22:17:02 +0700

    Create  member card component

---
+++ src/components/memberCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Image from "next/image";
+import * as styles from '@/components/styles'
+
+const MemberCard = ({src, name, position}) => {
+  return (
+    <div>
+        <Image src={src} alt={name} width={500} className="mb-2"/>
+        <div className="gap-1">
+            <h3 className={`${styles.display} m-0 p-0 w-fit`}>{name}</h3>
+            <p className={`${styles.subheader}  m-0 p-0 w-fit text-gray-400`}>{position}</p>
+        </div>
+    </div>
+  )
+};
+
+export default MemberCard;
