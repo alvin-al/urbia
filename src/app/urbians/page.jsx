d047edf1d3de0d855a0071f7905ac1d9126fc87e
Alvin Al <alvvxal@gmail.com>
2024-04-29 06:54:40 +0700

    Add hover effect on image person

--- src/app/urbians/page.jsx
+++ src/app/urbians/page.jsx
@@ -12,7 +12,7 @@ import devDep from '@/../../public/images/partners-logo/dev-dep.svg'
 import urbLiving from '@/../../public/images/partners-logo/urb-living.svg'
 import urbiaLogo from '@/../../public/images/partners-logo/urbia.svg'
 import Affordability from "@/components/affordability";
-
+import arrow from "@/../../public/icons/arrow-icon.svg"
 
 const Urbians = () => {
   const members = [
@@ -35,18 +35,18 @@ const Urbians = () => {
   const keterjangkauan = [
     {
       title: 'People',
-      content: 'Urbia mengedepankan manifestasi ruang yang berfokus pada integrasi dari konteks dan kebutuhan pengguna ruang. Dalam mengolah kebutuhan desain, Urbia memandang klien sebagai kolaborator',
-      number: '+365'
+      content: 'Memulai dari tahun 2020, Urbia telah berkontribusi dengan klien, vendor, kontraktor, studio, workshop, partner, dan pihak lain yang bersama-sama mewujudkan berbagai project, ide dan gagasan, bisnis, hingga project terbangun. Berbagai jenis klien dengan berbagai kebutuhannya telah terakomodasi.',
+      number: '+47'
     },
     {
       title: 'Locations',
-      content: 'Urbia mengedepankan manifestasi ruang yang berfokus pada integrasi dari konteks dan kebutuhan pengguna ruang. Dalam mengolah kebutuhan desain, Urbia memandang klien sebagai kolaborator',
-      number: '+5'
+      content: 'Urbia telah mengerjakan project di berbagai lokasi. Masing-masing memiliki konteks dan masalah yang berbeda. Urbia memperhatikan kondisi lokasi dimulai dari mikro site hingga makro kawasan/daerah dimana lokasi project berada.',
+      number: '+16'
     },
     {
       title: 'Project',
-      content: 'Urbia mengedepankan manifestasi ruang yang berfokus pada integrasi dari konteks dan kebutuhan pengguna ruang. Dalam mengolah kebutuhan desain, Urbia memandang klien sebagai kolaborator',
-      number: '+40'
+      content: 'Urbia telah mengerjakan project di berbagai lokasi. Masing-masing memiliki konteks dan masalah yang berbeda. Urbia memperhatikan kondisi lokasi dimulai dari mikro site hingga makro kawasan/daerah dimana lokasi project berada.',
+      number: '+28'
     }
   ];
 
@@ -82,7 +82,7 @@ const Urbians = () => {
           </div>
         </div>
 
-        <div className="flex flex-col mt-48 gap-12 lg:gap-20 xl:gap-32 justify-center flex-wrap md:justify-between md:flex-nowrap md:flex-row pb-32 xl:px-24 lg:pb-64 lg:px-12">
+        <div className="flex flex-col mt-48 gap-12 lg:gap-20 xl:gap-16 justify-center flex-wrap md:justify-between md:flex-nowrap md:flex-row pb-32 xl:px-24 lg:pb-64 lg:px-12">
           {members.map((member, index) => (
             <MemberCard key={index} name={members[index].name} src={members[index].src} position={members[index].position} />
           ))
@@ -102,7 +102,7 @@ const Urbians = () => {
         <div id="urbians-data" className="mb-20">
           <div className="border-t border-black pt-8 pb-24 flex gap-4 flex-col">
             <h1 className={`${styles.headpoints5xl}`}>A team made of experts</h1>
-            <p className="md:w-1/2">Urbia mengedepankan manifestasi ruang yang berfokus pada integrasi dari konteks dan kebutuhan pengguna ruang.</p>
+            <p className="md:w-1/2">Urbia tidak bekerja sendiri, klien, vendor, kontraktor, studio, workshop, partner, dan pihak lain juga berpartisipasi dalam keberhasilan project. Tentu saja, Urbia memiliki partner dan relasi dengan latar belakang profesional dan terpercaya. Keterbukaan menjadi kunci dalam proses Urbia mengakomodasi kebutuhan klien. </p>
           </div>
           <div>
             {keterjangkauan.map((content, index) => (
@@ -114,6 +114,29 @@ const Urbians = () => {
 
           </div>
         </div>
+
+        <div id="contact-us" className="">
+          <h1 className={`${styles.headpoints5xl} mb-4`}>This is how we start the party</h1>
+          <p className="md:w-1/2">Feel free to ask everything to us!</p>
+          <div className="py-4">
+            <div className="flex flex-col gap-4">
+              <input type="text" placeholder="[  Silakan tulis nama Anda ...  ]" className="w-full border-b-2 border-gray-800"/>
+              <input type="email" placeholder="[  Silakan tulis e-mail Anda ...  ]" className="w-full border-b-2 border-gray-800"/>
+              <input type="text" placeholder="[  Silakan beritahu kami, project atau apapun yang bisa kami kontribusikan untuk Anda ...  ]" className="flex w-full h-64 placeholder:text-wrap placeholder:text-start m-0 p-0 text-start border-b-2 border-gray-800"/>
+
+              <button type="submit" className="w-fit flex gap-4 items-center m-auto">
+                <p className="hover:underline hover:underline-offset-2">Let’s Collaborate</p>
+                <div className="w-12 ">
+                  <Image src={arrow} alt="arrow-icon" className=""/>
+                </div>
+              </button>
+            </div>
+              <p className="md:w-1/2 text-sm mt-4 text-center">Anda akan mendapatkan balasan email dari kami, segera.</p>
+            <div>
+            </div>
+          </div>
+        </div>
+
 
         <Footer className='items-end' />
     </div>
--- src/components/memberCard.jsx
+++ src/components/memberCard.jsx
@@ -5,7 +5,14 @@ import * as styles from '@/components/styles'
 const MemberCard = ({src, name, position}) => {
   return (
     <div className="">
-        <Image src={src} alt={name} width={1000} className="mb-2"/>
+          <div className="xl:w-[400px] relative">
+            <div className="relative overflow-hidden">
+              <Image src={src} alt={name} width={1000} className="mb-2 border hover:-translate-y-32 transition-transform duration-300"/>
+            <div className="absolute top-0 left-0 w-full h-full flex items-center bg-white opacity-0 hover:opacity-100 hover:bg-opacity-85 transition-opacity duration-300">
+              <p className="text-black text-sm px-10">Yusuf tertarik dalam mengamati dan berkecimpung pada pelestarian dan pemanfaatan bangunan heritage hingga saat ini. Studi yang dilakukan Yusuf menemukan bahwa pemanfaatan bangunan cagar budaya dapat menghasilkan nilai komersil dan pelestarian secara bersamaan. Konsentrasi Yusuf pada urban governance, menurutnya dapat memberikan dampak yang baik pada pelestarian dan perkembangan arsitektur dan kota.</p>
+            </div>
+            </div>
+          </div>
         <div className="gap-1">
             <h3 className={`${styles.display} m-0 p-0 w-fit`}>{name}</h3>
             <p className={`${styles.subheader}  m-0 p-0 w-fit text-gray-400`}>{position}</p>
