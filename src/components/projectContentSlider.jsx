97a29c94a3b2f5b0796ab00eac67ee96814a9614
alvin-al <alvvxal@gmail.com>
2024-06-20 20:06:31 +0700

    Remove project carousel

--- src/components/projectContentSlider.jsx
+++ src/components/projectContentSlider.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import HomeCarousel from "./carousel";
-import ProjectCarousel from "./projectCarousel";
 
 const ProjectContentSlider = ({ category }) => {
   return (
     <div className="w-[80%]">
-        {/* <HomeCarousel /> */}
-        <ProjectCarousel />
+        <HomeCarousel />
     </div>   
     );
 };
