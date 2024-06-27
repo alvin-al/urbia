c36c67944cb77dce89c64d40096a4f6854e77623
alvin-al <alvvxal@gmail.com>
2024-06-27 18:31:53 +0700

    Fix font

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,8 @@ import { Inter, Fraunces, Raleway } from "next/font/google";
 import "./globals.css";
 
 const raleway = Raleway({
-  subsets: ['latin'],
+  subsets: ["latin"],
+  weight: ["100", "200","300", "400","500","600", "700"],
   display: 'swap',
 });
 
--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -8,10 +8,24 @@ import Footer from "@/components/footer";
 import ProjectSidebar from "@/components/projectSidebar";
 import Link from "next/link"
 import ProjectContentSlider from "@/components/projectContentSlider";
+import pic1 from '@/../public/images/AJ1.webp'
+import pic2 from '@/../public/images/Giorno.png'
+import pic3 from '@/../public/images/living room lt.1.webp'
+import pic4 from '@/../public/images/XT-NOON-3.webp'
 
 const Projects = () => {
   const [category, setCategory] = useState('Housing');
-
+  
+  const OPTIONS = { loop: true }
+  const SLIDES = {
+  'Housing': [{ src: pic1 }, { src: pic2 }, { src: pic3 }],
+  'Commercial': [{ src: pic3 }, { src: pic4 }],
+  'Restaurant/Cafe': [{ src: pic3 }, { src: pic2 }],
+  'Public Space': [{ src: pic2 }, { src: pic4 }],
+  'Guesthouse/Villa': [{ src: pic1 }, { src: pic4 }],
+  'Office': [{ src: pic2 }, { src: pic3 }],
+  'default': [{ src: pic1 }, { src: pic2 }, { src: pic3 }, { src: pic4 }]
+};
 
   return(
     <div className={`${styles.pageSize} flex h-[120vh] z-0`}>
@@ -21,7 +35,11 @@ const Projects = () => {
         </p>
         <div id="projects" className="flex justify-center gap-5 ">
           <ProjectSidebar category={category} setCategory={setCategory}/>
-          <ProjectContentSlider category={category}/>
+          <ProjectContentSlider 
+            category={category} 
+            slides={SLIDES[category] || SLIDES.default} 
+            options={OPTIONS} 
+          />
         </div>
 
         <div className='bottom-0 fixed'>
