22bbc30d3981a0c4cef8782e8728e8f776a53a8a
alvin-al <alvvxal@gmail.com>
2024-09-05 06:13:56 +0700

    Change project page into component

--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Header from "@/components/header";
 import * as styles from "@/components/styles";
 import Image from "next/image";
@@ -8,6 +8,7 @@ import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import client from "@/lib/contentful";
+import { PuffLoader } from "react-spinners";
 
 const ProjectPage = () => {
   const pathname = usePathname();
@@ -16,42 +17,54 @@ const ProjectPage = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    client.getEntries({
-      "content_type": "projects",
-      "fields.slug": slug,
-    })
-    .then((response) => {
-      if (response.items.length > 0) {
-        setPost(response.items[0]);
+    client
+      .getEntries({
+        "content_type": "projects",
+        "fields.slug": slug,
+      })
+      .then((response) => {
+        if (response.items.length > 0) {
+          setPost(response.items[0]);
+          setLoading(false);
+        } else {
+          setPost(null);
+          setLoading(false);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching post:", error);
         setLoading(false);
-      } else {
-        setPost(null);
-        setLoading(false);
-      }
-    })
-    .catch((error) => {
-      console.error("Error fetching post:", error);
-      setLoading(false);
-    });
-  }, [client, slug]);
+      });
+  }, [slug]);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className='w-full h-[100vh] justify-center items-center flex'>
+        <PuffLoader />
+      </div>
+    );
   }
 
   if (!post) {
     return <div>Post not found.</div>;
   }
 
+  console.log(post);
+
   const mainImageUrl = post.fields.mainImage.fields.file.url;
-  const imageGallery1 = post.fields.projectGallery[0].fields.file.url;
-  const imageGallery2 = post.fields.projectGallery[1].fields.file.url;
 
   return (
     <div className={`${styles.pageSize} flex border-2`}>
       <Header title={post.fields.title} />
       <meta name='description' content={post.fields.title}></meta>
-      <Image src={`https:${mainImageUrl}`} alt={post.fields.title} className='relative w-full md:h-[75vh] object-cover rounded-b-[36px]' width={1000} height={1000} />
+      <Image
+        src={`https:${mainImageUrl}`}
+        alt={post.fields.title}
+        className='relative w-full md:h-[75vh] object-cover rounded-b-[36px]'
+        width={1000}
+        height={1000}
+        priority={true}
+      />
       <div className='mt-4 mb-12'>
         <h1 className={`${styles.headpoints5xl} uppercase`}>
           {post.fields.title}
@@ -110,31 +123,55 @@ const ProjectPage = () => {
         <div className='w-1/2 pr-36 pl-12 pt-12 flex flex-col gap-12'></div>
       </div>
       <div className='w-1/2 h-fit mb-8'>
-        <Image src={`https:${imageGallery1}`} alt='giorno' className='object-cover h-96' width={500} height={500}/>
+        <Image
+          src={`https:${post.fields.projectGallery[0].fields.file.url}`}
+          alt='giorno'
+          className='object-cover h-96'
+          width={500}
+          height={500}
+        />
       </div>
       <div className='flex h-fit w-full gap-4 mb-8'>
         <div>
-        <Image src={`https:${imageGallery2}`} alt='giorno' className='h-64 object-cover' width={500} height={500}/>
+          <Image
+            src={`https:${post.fields.projectGallery[1].fields.file.url}`}
+            alt='giorno'
+            className='h-64 object-cover'
+            width={500}
+            height={500}
+          />
         </div>
         <div>
-          <Image src={giorno} alt='giorno' className='h-64 object-cover' width={500} height={500}/>
+          <Image
+            src={giorno}
+            alt='giorno'
+            className='h-64 object-cover'
+            width={500}
+            height={500}
+          />
         </div>
         <div>
-          <Image src={giorno} alt='giorno' className='h-64 object-cover' width={500} height={500}/>
+          <Image
+            src={giorno}
+            alt='giorno'
+            className='h-64 object-cover'
+            width={500}
+            height={500}
+          />
         </div>
       </div>
       <div className='w-full h-fit mb-8 flex gap-4'>
-        <div className='object-cover w-1/2' >
-        <Image src={giorno} alt='giorno'/>
+        <div className='object-cover w-1/2'>
+          <Image src={giorno} alt='giorno' />
         </div>
-        <div className='object-cover w-1/2' >
-        <Image src={giorno} alt='giorno'/>
+        <div className='object-cover w-1/2'>
+          <Image src={giorno} alt='giorno' />
         </div>
       </div>
       <div className='w-full h-fit mb-8 flex gap-4  '>
         <div className='w-1/2'></div>
-        <div className='object-cover h-96 w-1/2' >
-        <Image src={giorno} alt='giorno'/>
+        <div className='object-cover h-96 w-1/2'>
+          <Image src={giorno} alt='giorno' />
         </div>
       </div>
       <div className=''>
