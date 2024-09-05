a7110fcba51e0bfd0d11871247f82bd0a744bfa7
alvin-al <alvvxal@gmail.com>
2024-09-05 22:02:11 +0700

    Add tooltip hover on homepage slider

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,98 +1,76 @@
-'use client'
+"use client";
 
-import Image from "next/image"
+import Image from "next/image";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/components/ui/carousel"
-import { useState } from "react"
-import pic1 from '@/../public/images/AJ1.webp'
-import pic2 from '@/../public/images/Giorno.png'
-import pic3 from '@/../public/images/living room lt.1.webp'
-import pic4 from '@/../public/images/XT-NOON-3.webp'
-import Autoplay from 'embla-carousel-autoplay'  
-import client from "@/lib/contentful"
-import { useEffect } from "react"
-import Link from "next/link"
-import { Skeleton } from "@/components/ui/skeleton"
-
+} from "@/components/ui/carousel";
+import { useState } from "react";
+import pic1 from "@/../public/images/AJ1.webp";
+import pic2 from "@/../public/images/Giorno.png";
+import pic3 from "@/../public/images/living room lt.1.webp";
+import pic4 from "@/../public/images/XT-NOON-3.webp";
+import Autoplay from "embla-carousel-autoplay";
+import client from "@/lib/contentful";
+import { useEffect } from "react";
+import Link from "next/link";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const HomeCarousel = () => {
   const [post, setPost] = useState(null);
   const [loading, setLoading] = useState(true);
 
+  client
+    .getEntries()
+    .then((response) => {
+      setPost(response.items);
+      setLoading(false);
+    })
+    .catch(console.error);
 
-  client.getEntries()
-  .then((response) => {
-    setPost(response.items);
-    setLoading(false);
-})
-.catch(console.error)
-
-
-  const listOfPic = [
-    {
-      name : 'AJ House',
-      link : pic1
-    },
-    {
-      name : 'Giorno Pizzeria',
-      link : pic2
-    },
-    {
-      name : 'Casa Banjar',
-      link : pic3
-    },
-    {
-      name : 'Arahiwang',
-      link : pic4
-    }
-  ]
-
-  if(loading){
+  if (loading) {
     return (
-      <div className="flex flex-col h-[72vh] 2xl:h-[75vh] rounded-xl my-2">
-        <Skeleton className="w-full h-[72vh] 2xl:h-[75vh]" />
+      <div className='flex flex-col h-[72vh] 2xl:h-[75vh] rounded-xl my-2'>
+        <Skeleton className='w-full h-[72vh] 2xl:h-[75vh]' />
       </div>
-    )
+    );
   }
 
-
   return (
-    <div className="rounded-lg my-2">
-      <Carousel 
+    <div className='rounded-lg my-2'>
+      <Carousel
         opts={{
           loop: true,
-          }} 
+        }}
         plugins={[
-            Autoplay({
-              delay: 3000,
-            }),
-          ]}
-        className="flex flex-col h-[72vh] 2xl:h-[74vh] rounded-xl overflow-hidden"
-        >
+          Autoplay({
+            delay: 3000,
+          }),
+        ]}
+        className='flex flex-col h-[72vh] 2xl:h-[74vh] rounded-xl overflow-hidden'
+      >
         <CarouselContent>
-          {/* <CarouselItem><Image src={pic1} alt="" className="object-cover w-full h-[73vh] xl:h-[76vh] object-bottom hover:cursor-pointer"/></CarouselItem>
-          <CarouselItem><Image src={pic2} alt="" className="object-cover w-full h-[73vh] xl:h-[76vh] object-bottom hover:cursor-pointer"/></CarouselItem>
-          <CarouselItem><Image src={pic3} alt="" className="object-cover w-full h-[73vh] xl:h-[76vh] object-bottom hover:cursor-pointer"/></CarouselItem> */}
-
           {post.map((item) => (
-            <CarouselItem key={item.sys.id} className="hover:cursor-pointer">
+            <CarouselItem key={item.sys.id} className='hover:cursor-pointer'>
               <Link href={`/projects/${item.fields.slug}`} key={item.sys.id}>
-                <Image src={`https:${item.fields.mainImage.fields.file.url}`} 
-                alt={item.fields.mainImage.fields.description || "image"} 
-                className="object-cover w-full h-[72vh] 2xl:h-[75vh] object-bottom hover:cursor-pointer" width={1000} height={1000}/>
+                <Image
+                  src={`https:${item.fields.mainImage.fields.file.url}`}
+                  alt={item.fields.mainImage.fields.description || "image"}
+                  title={item.fields.title || "image"}
+                  className='object-cover w-full h-[72vh] 2xl:h-[75vh] object-bottom hover:cursor-pointer'
+                  width={1000}
+                  height={1000}
+                />
               </Link>
             </CarouselItem>
-))}
+          ))}
         </CarouselContent>
       </Carousel>
-      
     </div>
-  )
-}
+  );
+};
 
 export default HomeCarousel;
